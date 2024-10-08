114bdf94f1224e83c0ec6dafba798b7d02c50df7
MTHSKM <matheuskim356@gmail.com>
2024-10-08 13:07:36 -0300

    Estilos Globais

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -11,7 +11,16 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      precato: "#164c81",
+      white: "#FFF",
+
+      gray900: "#121214",
+      gray800: "#202024",
+      gray300: "#c4c4cc",
+      gray100: "e1e1ec",
+
+      blue700: "#123d68",
+      blue500: "#164c81",
+      blue300: "#386593",
     },
   },
 });
