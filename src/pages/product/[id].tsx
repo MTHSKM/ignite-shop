55e4e8d3d9eb16811b9621c94a088d8acc233a3f
MTHSKM <matheuskim356@gmail.com>
2024-10-10 12:58:53 -0300

    Fallback do SSG

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -6,6 +6,7 @@ import {
 } from "@/styles/pages/product";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import Stripe from "stripe";
 
 interface ProductProps {
@@ -19,6 +20,12 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <p> Loading... </p>;
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -39,8 +46,8 @@ export default function Product({ product }: ProductProps) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [{ params: { id: "prod_R023Edw4FmBYAM" } }],
-    fallback: false,
+    paths: [],
+    fallback: true,
   };
 };
 
