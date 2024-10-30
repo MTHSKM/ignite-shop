dabd419f948882ea4be58e21315886282816c202
MTHSKM <matheuskim356@gmail.com>
2024-10-30 14:24:58 -0300

    botao de comprar

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,8 +1,17 @@
 import { stripe } from "@/lib/stripe";
+import { error } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const priceId = 'price_1Q821AIES6TUGSZaJrXgJ2Rm'
+  const {priceId} = req.body
+
+  if(req.method !== 'POST') {
+    return res.status(405).json({error: "Method not allowed."})
+  }
+
+  if(!priceId) {
+    return res.status(400).json({error: "Price not found."})
+  }
 
   const success_url = `${process.env.NEXT_URL}/success`
   const cancel_url = `${process.env.NEXT_URL}`
